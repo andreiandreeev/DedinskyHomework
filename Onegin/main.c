add0ad8450b09d034e3307e8c78f3939d5adb55f
Andrei Andreev <43216646+andreijoj@users.noreply.github.com>
2018-10-03 20:52:48 +0300

    Update main.c

--- Onegin/main.c
+++ Onegin/main.c
@@ -1,75 +1,85 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <assert.h>
 #define INPUT "text"
 #define OUTPUT "sorted"
+
 char* fileToString(FILE* file, int* stringLength);
-char** stringToPtrArray(char* string, int stringLength, int* arrayLength);
-int stringCmp(char* a, char* b);
+char** stringToPtr(char* string, int stringLength, int* arrayLength);
+int stringCmp(const void* a, const void* b);
 
 int main()
 {
-  FILE* file = fopen(INPUT, "r");
-  char* stringFromFile;
-  char** ptrArray;
-  int stringLength;
-  int arrayLength;
+  FILE* input  = fopen(INPUT,  "r");
+  char* originalPoem;
+  char** originalLinkedPoem;
+  char** sortedLinkedPoem;
+  int strLength;
+  int arrayLength = 0;
+  
+  originalPoem = fileToString(input, &strLength);
+  originalLinkedPoem = stringToPtr(originalPoem, strLength, &arrayLength); 
 
-  stringFromFile = fileToString(file, &stringLength);
-  ptrArray = stringToPtrArray(stringFromFile, stringLength, &arrayLength);
- 
-  fclose(file);
-  file = fopen(OUTPUT, "w"); 
- /* qsort(ptrArray, arrayLength, sizeof(char*), stringCmp);
-  */
+  sortedLinkedPoem = stringToPtr(originalPoem, strLength, NULL);
+  qsort(sortedLinkedPoem, arrayLength, sizeof(char*), stringCmp);
+
   int i;
+  fclose(input);
+  FILE* outputFile = fopen(OUTPUT, "w");
+
   for(i = 0; i < arrayLength; i++)
-  {
-    fprintf(file, "%s\n", ptrArray[i]);
-  }
+    if(sortedLinkedPoem[i][0] != '\0') 
+      fprintf(outputFile, "%s\n", sortedLinkedPoem[i]);
   
-  free(stringFromFile);
-  free(ptrArray);
-  
-  fclose(file);
-  return 0;
+  for(i = 0; i < arrayLength; i++)
+    if(originalLinkedPoem[i][0] != '\0')
+      fprintf(outputFile, "%s\n", originalLinkedPoem[i]);
+
 }
 
 char* fileToString(FILE* file, int* stringLength)
 {
+  assert(file != NULL);
+  assert(stringLength != NULL);
+
   fseek(file, 0L, SEEK_END);
-  *stringLength = ftell(file) + 1;
+  *stringLength = ftell(file);
   fseek(file, 0L, SEEK_SET);
-  printf("%d", *stringLength);
   char* string = (char*)calloc(*stringLength, sizeof(char));
-  fread(string, *stringLength, 1, file);
-  
+  fread(string, *stringLength, 1, file); 
   return string;
 }
 
-char** stringToPtrArray(char* string, int stringLength, int* arrayLength)
+char** stringToPtr(char* string, int stringLength, int* arrayLength)
 {
-  int i;
+  int i = 0;
+  int iterator = 0;
   char** ptrArray;
-  *arrayLength = 0;
   
   for(i = 0; i < stringLength; i++)
     if(string[i] == '\n' || string[i] == '\0')
-      *arrayLength++;
-  
+      iterator++;
+  *arrayLength = iterator;
   ptrArray = (char**)calloc(*arrayLength, sizeof(char*));
   
-  int iterator = 0;
-  
+  iterator = 0;
+  ptrArray[0] = string;
   for(i = 0; i < stringLength; i++)
   {
-    if(string[i] == '\n' || string[i] == '\0')
+    if(string[i] == '\n')
     {
-      string[i] = '\0';
-      ptrArray[iterator++] = &string[i];
+      ptrArray[iterator++] = &string[i+1];
+      string[i] = '\0'; 
     }
   }
-  return ptrArray;
+  return ptrArray; 
 }
+
+int stringCmp(const void* a, const void* b)
+{
+  const char* pa = *(const char**)a;
+  const char* pb = *(const char**)b;
+  return strcasecmp(pa, pb);
+}
+ 
